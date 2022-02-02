012dd658837a8eb79a657cd5bb994c3777266db7
Storm Shadow <83181647+StormShadow058@users.noreply.github.com>
2022-02-02 16:54:21 +0530

    Initial Commit

---
+++ GettingUserLocation/GettingUserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingUserLocationApp.swift
+//  GettingUserLocation
+//
+//  Created by Vansh Maheshwari on 02/02/22.
+//
+
+import SwiftUI
+
+@main
+struct GettingUserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
